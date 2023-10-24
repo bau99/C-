caa00a38101785a89bfa529bc66158814bd69fda
Laughing111 <bau99@mail.ru>
2023-10-25 01:37:24 +0300

    ДЗ. Задача 29, c неформатированным выводом.

---
+++ ExamplesC#/TaskDZ13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+//!!! Задача не уточнена: как задаются значения элементов массива? 
+//    Примеры приведены некорректно условию: массивы 5 и 3 элементов!
+//    Поэтому массив заполним случайным образом, а задачу сделаем более универсальной.
+
+int Digits(string message)
+{
+    Console.WriteLine(message);
+    int digit = Convert.ToInt32(Console.ReadLine());
+    return digit;
+}
+
+void FillArray(int[] array, int min, int max )
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min,max+1);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+       Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+
+int arle = Digits("Введите размер массива: ");
+int start = Digits("Введите нижнюю границу значений элементов массива: ");
+int finish = Digits("Введите верхнюю границу значений элементов массива: ");
+int[] array = new int[arle];
+FillArray(array, start, finish);
+PrintArray(array);
