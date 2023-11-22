a7ccfb1e324db78991f7cc75c589126d61379684
Laughing111 <bau99@mail.ru>
2023-11-22 20:18:49 +0300

    ДЗ 7. Задача 1

---
+++ ExamplesC#/TaskUrok7_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+int Input(string message) 
+{ 
+    System.Console.Write(message); 
+    int result = Convert.ToInt32(Console.ReadLine()); 
+    return result; 
+}
+
+string RowNumbers(int min, int max)
+{
+    if (min <= max) return $"{min} " + RowNumbers(min+1, max);
+    else return String.Empty;
+}
+
+int m = Input("Введите первое число: ");
+int n = Input("Введите второе число: "); 
+
+if (m < n) System.Console.WriteLine(RowNumbers(m, n));
+else System.Console.WriteLine(RowNumbers(n, m));
